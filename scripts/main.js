535dece900aca5f1d3ec95842d6ae042f2352cba
Sandra Galilejeva <sandra.galilejeva@gmail.com>
2019-11-15 16:59:01 +0200

    added js scripts

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,18 @@
-console.log(Running My JS)
+
+
+console.log("RunningMyJS");
+a = 55;
+console.log(a);
+
+
+var myName = "Sandra";
+var myStr = "My name is " + myName + " and I am well!";
+console.log("Name");
+
+var someAdjective = "fascinating"
+var myStr = "Learning to code is ";
+myStr += someAdjective;
+console.log("myStr");
+
+var myPar = document.querySelector('#myResults');
+myPar > innerHTML="<em>" + myStr + "<em>";
