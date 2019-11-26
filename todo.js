9b488e960a8d9c335ac44dfacfb97c8ad70f3612
Sandra Galilejeva <sandra.galilejeva@gmail.com>
2019-11-26 12:03:32 +0200

    added button on click

--- todo.js
+++ todo.js
@@ -1,2 +1,30 @@
-console.log("Running TODO list");
+console.log("Running TODO APP!");
+
+const addBtn = document.getElementById('todo-add');
+
+addBtn.onclick = () => {
+    console.log("You clicked ADD");
+    const jobCont = document.createElement('div');
+    jobCont.id = 'job-' + jobiD
+
+
+    const jobDesc = document.createElement('p')
+    jobDesc.innerText = jobInput.value;
+
+
+    document.getElementById('todo-list').appendChild(el);
+}
+
+document.getElementById('delete-all').onclick = () => {
+    console.log("Deleting everything");
+    while (todoList.firstChild) {
+        todoList.removeChild(todoList.firstChild);
+    }
+}
+// const container = document.querySelectorAll('.container');
+// console.log("container tag" + container.tagName);
+// const children = container.childNodes;
+// for (let child of children) {
+    // console.log("My element has tag" + child.tagName);
+// }
 
