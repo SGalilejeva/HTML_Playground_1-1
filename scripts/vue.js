e53b6e35e4ca5d8c602a4e2cb0a11bf0d29da8b3
Sandra Galilejeva <sandra.galilejeva@gmail.com>
2019-11-28 12:31:03 +0200

    added div as vue element

--- scripts/vue.js
+++ scripts/vue.js
@@ -1,1 +1,12 @@
 console.log("Running on my Vue");
+
+let app = new Vue({
+    el: '#app',
+    data: {
+        message: 'Hello Rcs!',
+        secondmessage: "Hello again",
+        seen: true
+    }
+})
+
+const myval = 42 
