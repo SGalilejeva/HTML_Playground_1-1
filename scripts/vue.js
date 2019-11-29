5265b9d8c0842fcebf47db361efccb114f1e2359
Sandra Galilejeva <sandra.galilejeva@gmail.com>
2019-11-29 11:25:39 +0200

    added methods in vue

--- scripts/vue.js
+++ scripts/vue.js
@@ -6,12 +6,32 @@ let app = new Vue({
         message: 'Hello Rcs!',
         secondmessage: "Hello again",
         seen: true,
+        seenMsg: "Turn me OFF"
         todos: [
             { text: 'Learn JavaScript' },
             { text: 'Learn Vue' },
             { text: 'Build something awesome' }
         ]
-    }
-})
+    },
 
-const myval = 42 
+    // methods: {
+    //     reverseMessage: function () {
+    //         this.message = this.message.split('').reverse().join('')
+    //     },
+    // toggleSeen: function () {
+    //     this.seen = !this.seen;
+    //     if (this.seen) {
+    //         this.seenMsg = "Turn me OFF!";
+    //     }
+    //     else {
+    //         this.seenMsg = "Turn me On!";
+    //     }
+    // }
+}
+}
+)
+
+Vue.component('blog-post', {
+    props: ['title'],
+    template: '<h3>{{ title }}</h3>'
+})
